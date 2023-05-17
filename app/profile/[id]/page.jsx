624e5bde88c64d07fa564ef3d162359b10b1a8ee
mand8ate <mand8ate@gmail.com>
2023-05-17 16:43:56 +0900

    change typo

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
-import Profile from "@components/profile";
+import Profile from "@components/Profile";
 
 const MyProfile = ({ params }) => {
   const searchParams = useSearchParams();
